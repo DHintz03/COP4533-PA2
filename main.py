edc45d36d92b96535cbe7cd36b264921f7697829
DHintz03 <davidhintz@ufl.edu>
2026-03-04 19:33:48 -0500

    FIFO functionality

--- main.py
+++ main.py
@@ -5,20 +5,64 @@ from pathlib import Path
 from typing import List
 
 
+def first_in_first_out(k: int, m: int, requests: List[int]) -> int:
+    cache = []
+    cache_size = 0
+    misses = 0
+    hits = 0
 
+    for r in requests:
+        found = False
+        for c in cache:
+            if c[0] == r:
+                hits += 1
+                found = True
+            c[1] += 1
+        if not found:
+            misses += 1
+            if cache_size < k:
+                cache_size += 1
+                cache.append([r, 0])
+            else:
+                first_index = 0
+                max = 0
+                for i in range(len(cache)):
+                    if cache[i][1] > max:
+                        max = cache[i][1]
+                        first_index = i
+                cache[first_index][0] = r
+                cache[first_index][1] = 0
+    return misses
 
 
 
 def read_input() -> tuple[int, int, List[int]]:
     # If an input file exists under ./cache_input, use the first one.
-    input_file = Path("match_input/input.in")
-    lines = input_file.read_text().splitlines()
-
+    file = open("cache_input/input.in", "r")
+    line_0 = file.readline()
+    line_1 = file.readline()
+    k, m = line_0.strip().split()
+    str_requests = line_1.strip().split()
+    int_requests = []
+    for r in str_requests:
+        int_requests.append(int(r))
+    return int(k), int(m), int_requests
 
 
 def main() -> None:
     #Main file
     k, m, requests = read_input()
+    print(k)
+    print(m)
+    print(requests)
+
+    fifo = first_in_first_out(k, m, requests)
+    lru = 0
+    optff = 0
+
+    print("FIFO \t:", fifo)
+    print("LRU  \t:", lru)
+    print("OPTFF\t:", optff)
 
 
 if __name__ == "__main__":
